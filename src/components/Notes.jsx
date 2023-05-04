b5d6d3f30958914a750010caf9a32c788d0666a2
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2023-05-04 14:52:53 +0530

    add createNote component

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,12 +1,17 @@
 import { React, useState, useEffect } from "react";
 import "../Note.css";
-import Note from "../Note";
+import Note from "./Note";
+import CreateNote from "./CreateNote";
 function Notes() {
+  //states
+  const [notes, setNotes] = useState([]);
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="notes">
       <Note />
       <Note />
-      <Note />
+      <CreateNote />
     </div>
   );
 }
