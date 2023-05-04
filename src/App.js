4419ff806c90d2feeaede645712be214d6e01ea5
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2023-05-04 14:51:03 +0530

    add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Notes from "./components/Notes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Notes />
+    </div>
+  );
 }
 
 export default App;
